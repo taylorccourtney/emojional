c6518249069de31f3cbc0163e455d9eca0d94ad6
Taylor <apple@Apples-MacBook-Pro-45.local>
2019-07-26 10:21:03 -0400

    Iteration1

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func showMessage(sender : UIButton) {
+      // Sends the inputed message to the button, so when the user clicks the button whatever message is inputed in the code will appear.
+        let alertController = UIAlertController(title: "Something to think about", message: "Why do we drive on parkways but park in driveways?", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Continue", style: UIAlertAction.Style.default, handler: nil))
+       
+        present(alertController, animated: true, completion : nil)
+        
+   
+}
+        
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +28,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
